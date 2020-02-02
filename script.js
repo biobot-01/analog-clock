dd77444c9e02e2addac5f69cb4bfc584e150c35b
Kevin Hattingh <kevin.hattingh@sintrex.com>
2020-02-02 22:31:34 +0200

    Display hands to show the current time

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const HOURHAND = document.querySelector('#hour');
 const MINUTEHAND = document.querySelector('#minute');
 const SECONDHAND = document.querySelector('#second');
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
-
 let date = new Date();
 
 console.log(date);
@@ -15,6 +11,10 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 
 console.log(`Hour: ${hr} Minute: ${min} Second(s): ${sec}`);
+
+let hrPosition = (hr*360 / 12) + (min*(360/60) / 12);
+let minPosition = (min*360 / 60) + (sec*(360/60) / 60);
+let secPosition = sec*360 / 60;
 
 HOURHAND.style.transform = `rotate(${hrPosition}deg)`;
 MINUTEHAND.style.transform = `rotate(${minPosition}deg)`;
