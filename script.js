8e6c9caafa7bf92cc5901316910bea15672efd3f
Kevin Hattingh <kevin.hattingh@sintrex.com>
2020-02-02 22:22:25 +0200

    Get the local time

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
 
+let date = new Date();
+
+console.log(date);
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+console.log(`Hour: ${hr} Minute: ${min} Second(s): ${sec}`);
+
 HOURHAND.style.transform = `rotate(${hrPosition}deg)`;
 MINUTEHAND.style.transform = `rotate(${minPosition}deg)`;
 SECONDHAND.style.transform = `rotate(${secPosition}deg)`;
